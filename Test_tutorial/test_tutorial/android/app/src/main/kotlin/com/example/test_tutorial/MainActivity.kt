e4465c51fa4ac512b4ff40bb950cd0208b87a254
jinarma <jinarma@gmail.com>
2022-03-20 15:23:16 +0530

    initial commit

---
+++ Test_tutorial/test_tutorial/android/app/src/main/kotlin/com/example/test_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Unique_Student_ID/unique_student_id_access/android/app/src/main/kotlin/com/example/unique_student_id_access/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.unique_student_id_access
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
